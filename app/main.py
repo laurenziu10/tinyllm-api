1a86c0247c03a3becec985cea101ef85c4866437
Laurenziu Biemel <laurenziubiemel@gmail.com>
2025-05-08 15:17:03 +0200

    Update GUI, Landing Page & Routing (main.py) für AG News Klassifikation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from model_handler import classify_text
+import os
+from datasets import load_dataset
+import random
 
 app = Flask(__name__)
 
@@ -16,6 +19,22 @@ def predict():
         "input": text
     })
 
+@app.route("/gui")
+def gui():
+    return send_from_directory(os.path.dirname(__file__), "gui.html")
+
+@app.route("/")
+def index():
+    return send_from_directory(os.path.dirname(__file__), "index.html")
+
+@app.route("/random")
+def random_sample():
+    dataset = load_dataset("ag_news", split="test")
+    sample = random.choice(dataset)
+    return jsonify({
+        "text": sample["text"],
+        "label": sample["label"]
+    })
+
 if __name__ == "__main__":
-    # Run-Button in VS Code kompatibel
     app.run(debug=True, host="0.0.0.0", port=5050)
